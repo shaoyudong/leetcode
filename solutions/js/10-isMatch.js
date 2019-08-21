636e8bbfbc9845cb0155bb8daa596a2dc5576d6a
shaoyudong <644589460@qq.com>
2019-08-21 21:11:42 +0800

    commit

---
+++ solutions/js/10-isMatch.js
@@ -0,0 +1,9 @@
+var isMatch = function(s, p) {
+
+}
+
+var s = 'aa';
+
+var p = 'a';
+
+console.log(isMatch(s, p));
--- solutions/js/8-myAtoi.js
+++ solutions/js/8-myAtoi.js
@@ -1,3 +1,11 @@
 var myAtoi = function(str) {
-    
-}
+    if (str === '+' || str === '-') return 0;
+    if (!/\d/.test(str)) return +str || 0;
+    str = str.trim();
+    if (/^[^\d+-]/.test(str)) return 0;
+    var res = parseInt(str)
+    if (res !== res) return 0;
+    res = res > 2147483647 ? 2147483647 : res;
+    res = res < -2147483648 ? -2147483648 : res;
+    return res;
+};
